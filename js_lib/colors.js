4830c9dba925840eade5c08f751ab41f64e2a217
ellie <ellie.harmon@gmail.com>
2014-04-12 19:53:52 -0700

    pulling updates from master

--- js_lib/colors.js
+++ js_lib/colors.js
@@ -1,152 +1,5 @@
-c_brown = "#8c564b";
-c_orange = "#ff7f0e";
-c_red = "#d62728";
-c_pink = "#e377c2";
-c_purple = "#9467bd";
-c_blue = "#1f77b4";
-c_turq = "#17becf";
-c_green = "#2ca02c";
-
-c2_blue = "#51ACEB";
-c2_green = "#94C93D";
-c2_yellow = "#"
-c2_orange = "#F1AA4D";
-c2_red = "#F77064";
-c2_pink = "#FF78C2";
-c2_purple = "#9263F6";
-
-c2_blue = "#53B0F0";
-c2_green = "#94C93D";
-c2_yellow = "#fbd400"
-c2_orange = "#f89e35";
-// c2_red = "#d96652";
-c2_red = "#f6745e"
-c2_pink = "#F897D3";
-// c2_purple = "#7B76E4";
-c2_purple = "#A18DF9"
-
-
-c3_blue =  "#3563AB";
-c3_green = "#6a9129";
-c3_yellow = "#F2b940";
-// c3_orange = "#C98411";
-c3_orange = c2_orange;
-c3_red = "#c82e18";
-c3_pink = c2_pink;
-c3_purple = "#582BC5";
-
-c3_blue = "#2B6DAF";
-c3_green = "#427B02";
-c3_yellow = "#F9BE46";
-c3_orange = "#F8A949";
-c3_red = "#B63835";
-c3_pink = "#E770B9";
-c3_purple = "#4C3AC6";
-
-c_grey = "#999999";
-
-// var clusters_bright = {"Actinobacteria": c2_purple, 
-// "Acidobacteria": c2_blue,
-// "Firmicutes" : c2_red,
-// "Proteobacteria" : c2_green,
-// "Bacteroidetes" : c2_yellow,
-// // "Deinococcus-Thermus" : c2_orange,
-// "Gemmatimonadetes" : c2_pink,
-// "Other" : c2_orange,
-// "No Hit" : c_grey};
-
-
-
-//var cb = ['rgb(27,158,119)','rgb(217,95,2)','rgb(117,112,179)','rgb(231,41,138)','rgb(102,166,30)','rgb(230,171,2)','rgb(166,118,29)'];
-
-// var clusters_bright = {
-// 	"Actinobacteria": cb[2], 
-// 	"Acidobacteria": c3_blue,
-// 	"Firmicutes" : cb[4],
-// 	"Proteobacteria" : cb[1],
-// 	"Bacteroidetes" : cb[6],
-// 	"Other" : cb[5],
-// 	"No Hit" : cb[3]
-// }
-
-// var cb = ['rgb(102,194,165)','rgb(252,141,98)','rgb(141,160,203)','rgb(231,138,195)','rgb(166,216,84)','rgb(255,217,47)','rgb(229,196,148)'];
-// 
-// var cb = ['rgb(141,211,199)','rgb(255,255,179)','rgb(190,186,218)','rgb(251,128,114)','rgb(128,177,211)','rgb(253,180,98)','rgb(179,222,105)','rgb(252,205,229)','rgb(217,217,217)']
-// 
-var clusters_bright = {
-	"Actinobacteria": 'rgb(128,177,211)', 
-	"Acidobacteria": 'rgb(251,128,114)',
-	"Firmicutes" : 'rgb(141,160,203)',
-	"Proteobacteria" : 'rgba(159, 209, 76, 1)', //'rgb(166,216,84)',
-	"Bacteroidetes" : 'rgb(253,180,98)',
-	"Other" : 'rgb(255,217,47)',
-	"No Hit" : "#bf986c"
-}
-
-// var clusters_bright = {
-//   "Actinobacteria": '#73a3cc', 
-//   "Acidobacteria": '#d85757',
-//   "Firmicutes" : '#b987c8',
-//   "Proteobacteria" : '#75a23f', //'rgb(166,216,84)',
-//   "Bacteroidetes" : '#ec9051',
-//   "Other" : '#fcca68',
-//   "No Hit" : "#bd7c47" //"#b65c47"
-// }
-
-// var base00 = "#151515";
-// var base01 = "#202020";
-// var base02 = "#303030";
-// var base03 = "#505050";
-// var base04 = "#b0b0b0";
-// var base05 = "#d0d0d0";
-// var base06 = "#e0e0e0";
-// var base07 = "#f5f5f5";
-// var base08 = "#ac4142";
-// var base09 = "#d28445";
-// var base0A = "#f4bf75";
-// var base0B = "#90a959";
-// var base0C = "#75b5aa";
-// var base0D = "#6a9fb5";
-// var base0E = "#aa759f";
-// var base0F = "#8f5536";
-
-// base 16 - mocha
-
-// var base00 = "#3B3228";
-// var base01 = "#534636";
-// var base02 = "#645240";
-// var base03 = "#7e705a";
-// var base04 = "#b8afad";
-// var base05 = "#d0c8c6";
-// var base06 = "#e9e1dd";
-// var base07 = "#f5eeeb";
-// var base08 = "#cb6077";
-// var base09 = "#d28b71";
-// var base0A = "#f4bc87";
-// var base0B = "#beb55b";
-// var base0C = "#7bbda4";
-// var base0D = "#8ab3b5";
-// var base0E = "#a89bb9";
-// var base0F = "#bb9584";
-
-// // base 16 - tomorrow
-// // 
-// var base00 = "#1d1f21";
-// var base01 = "#282a2e";
-// var base02 = "#373b41";
-// var base03 = "#969896";
-// var base04 = "#b4b7b4";
-// var base05 = "#c5c8c6";
-// var base06 = "#e0e0e0";
-// var base07 = "#ffffff";
-// var base08 = "#cc6666";
-// var base09 = "#de935f";
-// var base0A = "#f0c674";
-// var base0B = "#b5bd68";
-// var base0C = "#8abeb7";
-// var base0D = "#81a2be";
-// var base0E = "#b294bb";
-// var base0F = "#a3685a";
+// Colors modified from the tomorrow version of Chris Kempson's base16
+// http://chriskempson.github.io/base16/#tomorrow
 
 // // base 16 - tomorrow - mod
  
@@ -158,88 +11,38 @@ var base04 = "#b4b7b4";
 var base05 = "#c5c8c6";
 var base06 = "#e0e0e0";
 var base07 = "#ffffff";
-var base08 = "#cc6666";
-var base09 = "#de935f";
-var base0A = "#f0c674";
-var base0B = "#b0bb63"; //mod
-var base0C = "#8abeb7";
-var base0D = "#81a2be";
-var base0E = "#b294bb";
-var base0F = "#a67164"; //mod
-
-// base 16 - twilight - mod
-// 
-// var base00 = "#1e1e1e";
-// var base01 = "#323537";
-// var base02 = "#464b50";
-// var base03 = "#5f5a60";
-// var base04 = "#838184";
-// var base05 = "#a7a7a7";
-// var base06 = "#c3c3c3";
-// var base07 = "#ffffff";
-// var base08 = "#cf6a4c";
-// var base09 = "#cda869";
-// var base0A = "#ffcc66";
-// var base0B = "#8f9d6a";
-// var base0C = "#afc4db";
-// var base0D = "#7587a6";
-// var base0E = "#9b859d";
-// var base0F = "#9b703f";
+var base08 = "#cc6666"; // red
+var base09 = "#de935f"; // orange
+var base0A = "#f0c674"; // yellow
+var base0B = "#b0bb63"; // green (mod)
+var base0C = "#8abeb7"; // turq
+var base0D = "#81a2be"; // blue
+var base0E = "#b294bb"; // purple
+var base0F = "#a67164"; // brown (mod)
 
 
-// base 16 - eighties
-
-// var base00 = "#2d2d2d";
-// var base01 = "#393939";
-// var base02 = "#515151";
-// var base03 = "#747369";
-// var base04 = "#a09f93";
-// var base05 = "#d3d0c8";
-// var base06 = "#e8e6df";
-// var base07 = "#f2f0ec";
-// var base08 = "#f2777a";
-// var base09 = "#f99157";
-// var base0A = "#ffcc66";
-// var base0B = "#99cc99";
-// var base0C = "#66cccc";
-// var base0D = "#6699cc";
-// var base0E = "#cc99cc";
-// var base0F = "#d27b53";
-// 
 var clusters_bright = {
   "Actinobacteria": base0D, 
   "Acidobacteria": base08,
   "Firmicutes" : base0E,
-  "Proteobacteria" : base0B, // (0b on non-eighties; eighties: 0c'rgb(166,216,84)',
-  "Bacteroidetes" : base09, // 09 except on twilight, which 0c
+  "Proteobacteria" : base0B, 
+  "Bacteroidetes" : base09, 
   "Other" : base0A,
   "No Hit" : base0F
 }
 
 
 var sites_bright = {
-	"fire": c2_red, 
-	"on trail": c2_yellow,
-	"plant" : c2_green,
-	"water" : c2_blue,
-	"rocks" : c_grey,
-	"other" : c2_purple,
+	"fire": base08, 
+	"on trail": base0A,
+	"plant" : base0B,
+	"water" : base0D,
+	"rocks" : base03,
+	"other" : base0E,
 }
 
 var sites_key = {'S46': 'on trail', 'S36': 'on trail', 'S42': 'rocks', 'S31': 'on trail', 'S25': 'other', 'S16': 'other', 'S57': 'fire', 'S56': 'rocks', 'S55': 'water', 'S54': 'plant', 'S53': 'on trail', 'S52': 'water', 'S51': 'plant', 'S50': 'on trail', 'S15': 'on trail', 'S14': 'water', 'S59': 'plant', 'S58': 'plant', 'S19': 'plant', 'S18': 'other', 'S39': 'plant', 'S6': 'other', 'S13': 'water', 'S34': 'plant', 'S11': 'on trail', 'S10': 'rocks', 'S17': 'on trail', 'S30': 'on trail', 'S33': 'plant', 'S32': 'plant', 'S38': 'other', 'S22': 'fire', 'S23': 'fire', 'S20': 'fire', 'S47': 'plant', 'S26': 'on trail', 'S41': 'rocks', 'S24': 'plant', 'S43': 'rocks', 'S28': 'other', 'S29': 'plant', 'S62': 'fire', 'S48': 'water', 'S60': 'fire', 'S61': 'fire', 'S8': 'fire', 'S9': 'plant', 'S45': 'plant', 'S44': 'rocks', 'S1': 'plant', 'S2': 'on trail', 'S3': 'other', 'S4': 'other', 'S5': 'on trail', 'S49': 'water', 'S7': 'fire', 'S35': 'on trail', 'S21': 'fire', 'S12': 'other', 'S40': 'on trail', 'S37': 'other', 'S27': 'other'}
 
-
-var clusters_darker = {
-	"Actinobacteria": c3_orange, 
-	"Acidobacteria": c3_blue,
-	"Firmicutes" : c3_yellow,
-	"Proteobacteria" : c3_green,
-	"Bacteroidetes" : c3_red,
-	// "Deinococcus-Thermus" : c2_orange,
-//	"Gemmatimonadetes" : c3_pink,
-	"Other" : c3_purple,
-	"No Hit" : c3_pink
-}
 
 function drawLegend(colors) {
 
