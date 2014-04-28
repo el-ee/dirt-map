dca4b524e1c9ed9ae8ea18c3d303bbff91f74c7a
ellie <ellie.harmon@gmail.com>
2014-04-27 20:26:55 -0700

    updates for long heatmaps

--- js_lib/heatmap-shared.js
+++ js_lib/heatmap-shared.js
@@ -1,6 +1,7 @@
-var w_base = 20;
-var h_base = 1;
 
 var num_otus = 13856;
 var num_full = 1247;
 var num_trimmed = 580;
+
+var h_base = 1;
+var w_base = num_otus/num_trimmed*h_base; // this makes squares for the most zoomed in ones
